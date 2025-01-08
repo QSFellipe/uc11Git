f64836c2584243475a0030f0013e3950ee8b98fb
Fellipe Queiroz Silva <qsfellipe@gmail.com>
2025-01-08 09:49:53 -0300

    Alteração na classe conectDAO
    Alteração feita para efetuar a conexão com o Banco de Dados

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,30 +4,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
-    public Connection connectDB(){
+    public Connection connectDB() throws ClassNotFoundException{
         Connection conn = null;
+        String url = "jdbc:mysql://localhost:3306/cenaflix";
+        String user = "root";
+        String password = "Fal6250200407";
         
-        try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url,user,password);
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
     }
-    
 }
