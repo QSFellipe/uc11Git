bad9287c22af2b8a090088f3940fed37cdb3e1c2
Fellipe Queiroz Silva <qsfellipe@gmail.com>
2025-01-08 10:25:31 -0300

    Alteração ProdutosDAO
    Feita a implementação do método cadastrar

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,42 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
-import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
     Connection conn;
     PreparedStatement prep;
-    ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor) VALUES (?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.executeUpdate();
+
+        } catch (SQLException sqle) {
+            System.out.println("Erro: " + sqle.getMessage());
+        }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
         return listagem;
     }
-    
-    
-    
-        
 }
-
